f95369f64ecbeedb224feb24f932f44896fce52b
subhrendu@iitg.ernet.in <subhrendu@idrbt.ac.in>
2022-10-06 19:05:49 +0530

    modified:   second/app.js 	new file:   third/app.js 	new file:   third/demofile1.html

--- second/app.js
+++ second/app.js
@@ -1,7 +1,37 @@
 var http = require('http');
 
-//create a server object:
+// Create a server object
 http.createServer(function (req, res) {
   res.write('Hello World!'); //write a response to the client
   res.end(); //end the response
 }).listen(8080); //the server object listens on port 8080
+// http://127.0.0.1:8080
+
+
+// Create a server object with header
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Hello World!');
+  res.end();
+}).listen(8081);
+// http://127.0.0.1:8081
+
+
+// Read the Query String
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(req.url);
+  res.end();
+}).listen(8082);
+// http://127.0.0.1:8082/Subhrendu
+
+
+// Split the Query String
+var url = require('url');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var q = url.parse(req.url, true).query;
+  var txt = q.year + " " + q.month;
+  res.end(txt);
+}).listen(8083);
+// http://127.0.0.1:8083/?year=2022&month=October
